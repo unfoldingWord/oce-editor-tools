7c4d3faae6dd25bbabb5831b63334e191f8c25fd
larsgson <4819617+larsgson@users.noreply.github.com>
2023-04-11 15:28:02 +0200

    WIP add preview popup dialog

--- packages/html-usfm-pk/src/index.js
+++ packages/html-usfm-pk/src/index.js
@@ -1,6 +1,7 @@
 import PkUsfmEditor from "./components/PkUsfmEditor";
 import PkEditor from "./components/PkEditor";
 import PkPreview from "./components/PkPreview";
+import PrintPopupDialog from "./components/PrintPopupDialog";
 import PkCacheProvider from "./context/LocalPkCacheContext";
 import usePkBookImport from "./hooks/usePkBookImport";
 import useUnsavedDataState from "./hooks/useUnsavedDataState";
@@ -9,6 +10,7 @@ export {
   PkUsfmEditor,
   PkEditor,
   PkPreview,
+  PrintPopupDialog,
   PkCacheProvider,
   usePkBookImport,
   useUnsavedDataState,
