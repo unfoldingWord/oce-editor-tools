7bc0fee08f16e15c5a17d973af3752b085364455
larsgson <4819617+larsgson@users.noreply.github.com>
2023-05-23 14:50:18 +0200

    adapt netlify configuration

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    NPM_PACKAGE_VERSION: process.env.npm_package_version,
-    BUILD_NUMBER: require('child_process')
-      .execSync('git rev-parse --short HEAD')
-      .toString().trim(),
-  }
-}
