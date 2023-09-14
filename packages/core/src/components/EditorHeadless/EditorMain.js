fee3f7d87e66bd4124b7944c862c55cda2ac81f8
larsgson <4819617+larsgson@users.noreply.github.com>
2023-09-14 17:35:34 +0200

    safeguard against invalid sequenceIds - in EditorMain.js

--- packages/core/src/components/EditorHeadless/EditorMain.js
+++ packages/core/src/components/EditorHeadless/EditorMain.js
@@ -41,9 +41,32 @@ export function EditorMain({ components, ...props }) {
 
   const style = !sequenceIds ? { cursor: 'progress' } : {};
 
+  const validSeqIds = sequenceIds && htmlPerf
+
+  const allValidSeqIds = () => {
+  // This checking is no longer necessary once it is handled inside the HtmlPerfEditor 
+    let retVal = validSeqIds
+    if (validSeqIds) {
+      sequenceIds?.forEach(seqId => {
+        const isValid = (htmlPerf?.sequencesHtml)
+                          && (htmlPerf?.sequencesHtml[seqId] != null)
+        retVal = retVal && isValid
+        if (!isValid) {
+          console.log(`Invalid sequenceId ${seqId} in ${sequenceIds}`)
+          console.log(htmlPerf)
+          console.log(htmlPerf?.sequencesHtml)
+        }
+      })
+    }
+    return retVal
+  }
+
   return (
     <div key="1" className="Editor" style={style} ref={editorRef} {...props}>
-      {sequenceIds && htmlPerf ? (
+      {/* The below safeguarding 'allValidSeqIds' might become unnecessary, 
+          once this is handled inside the HtmlPerfEditor instead
+          and at that point it will be enough to just check 'validSeqIds'  */}
+      {allValidSeqIds() ? (
         <Highlighted
           target={highlighterTarget}
           options={highlighterOptions}
