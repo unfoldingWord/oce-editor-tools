ec21c1b438c8e2be2c5f96e5e9a9c6574af00351
Abel Perez <abelper54@gmail.com>
2023-09-20 08:14:55 -0400

    Headless improvements and graft editor

--- packages/core/src/components/EditorHeadless/Buttons/ButtonAlignmentBroken.jsx
+++ packages/core/src/components/EditorHeadless/Buttons/ButtonAlignmentBroken.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Popper } from '@mui/material';
 import { useEditorContext } from '../Editor';
 import { getFlatWordObj } from '../../../helpers/getFlatWordObj';
 import ButtonHeadless from './ButtonHeadless';
@@ -10,7 +9,6 @@ export function ButtonAlignmentBroken({
   component,
   onClick: _onClick,
 }) {
-  const [anchorEl, setAnchorEl] = useState(null);
   const [brokenAlignedWords, setBrokenAlignedWords] = useState();
   const { state } = useEditorContext();
   const { epiteleteHtml, bookCode } = state;
@@ -38,14 +36,11 @@ export function ButtonAlignmentBroken({
       }),
     [epiteleteHtml, bookCode]
   );
-  const openPopup = (event) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
+  const handleClick = (event) => {
     if (isFunction(_onClick)) _onClick(event);
   };
 
   const allAligned = !brokenAlignedWords || brokenAlignedWords.length === 0;
-  const popperOpen = Boolean(anchorEl);
-  const id = popperOpen ? 'simple-popper' : undefined;
 
   return (
     <>
@@ -53,22 +48,14 @@ export function ButtonAlignmentBroken({
         component={component}
         value="alignment"
         aria-label="show broken alignment"
-        onClick={openPopup}
+        onClick={handleClick}
         disabled={allAligned}
         allAligned={allAligned}
+        brokenAlignedWords={brokenAlignedWords}
         title="Alignment"
       >
         {children}
       </ButtonHeadless>
-      <Popper id={id} open={popperOpen} anchorEl={anchorEl}>
-        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
-          List of words with broken alignment:
-          <Box>
-            {brokenAlignedWords &&
-              brokenAlignedWords.map((str, i) => <li key={i}>{str}</li>)}
-          </Box>
-        </Box>
-      </Popper>
     </>
   );
 }
