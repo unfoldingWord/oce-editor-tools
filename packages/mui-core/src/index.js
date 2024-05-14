720944be416b7e500c9d2e6d4d49fcc6059cfcb2
larsgson <4819617+larsgson@users.noreply.github.com>
2024-05-14 12:00:31 +0200

    bumped beta versions

--- packages/mui-core/src/index.js
+++ packages/mui-core/src/index.js
@@ -1,6 +1,6 @@
 import Editor from "./components/Editor"
 import PrintDrawer from "./components/PrintDrawer"
-import { hasUnsavedData } from "../helpers/hasUnsavedData";
+import { hasUnsavedData } from "./helpers/hasUnsavedData";
 
 export { 
   PrintDrawer,
